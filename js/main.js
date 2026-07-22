ac0c3d0fd0d12b9dcccddecb7d59707d6c5d749c
timmdario <timmdario@users.noreply.github.com>
2026-07-22 09:39:00 +0200

    Move Foto Galerie to separate subpage fotogalerie.html

--- js/main.js
+++ js/main.js
@@ -438,22 +438,6 @@ function initScrollAnimations() {
             }
         }
     );
-
-    // Foto Galerie
-    gsap.fromTo('.foto-galerie-section',
-        { opacity: 0, y: 30 },
-        {
-            opacity: 1,
-            y: 0,
-            duration: 0.8,
-            ease: 'power2.out',
-            scrollTrigger: {
-                trigger: '.foto-galerie-section',
-                start: 'top 80%',
-                toggleActions: 'play none none none'
-            }
-        }
-    );
 }
 
 // ========================================
